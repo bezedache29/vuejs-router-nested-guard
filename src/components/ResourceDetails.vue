717265aa2ba16618283050637ef95eb82cae660b
Christophe Salou <bezedache29@gmail.com>
2021-04-17 12:05:59 +0200

    Stylisé page détails

--- src/components/ResourceDetails.vue
+++ src/components/ResourceDetails.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <p>Commentary de la resource : {{ resource.title }}</p>
+  <div class="details">
+    <h1>Détails de la ressource : {{ resource.title }}</h1>
+    <h3>Description :</h3>
+    <p>{{ resource.desc }}</p>
+    <h3>Commentaire :</h3>
     <p>{{ resource.commentary }}</p>
+    <div class="buttons">
+        <a target="_blank" :href="resource.link">{{ resource.link }}</a>
+      </div>
   </div>
 </template>
 
@@ -40,6 +46,35 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+  .details {
+    width: 50%;
+    margin: 0 auto;
+    padding: 0;
+    list-style-type: none;
+    border: 1px rgb(204, 204, 204) solid;
+    border-radius: 15px;
+    margin-top: 10px;
+  }
+  h1 {
+    text-align: center;
+  }
+  .buttons {
+    display: flex;
+    padding: 20px;
+    align-items: center;
+  }
+  .buttons a {
+    margin-right: 20px;
+  }
+  .buttons button {
+    background-color: red;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+  }
+  p, h3 {
+    padding-left: 20px;
+  }
 </style>

44e249866a01f82b3536d61310033a61d890daac
Christophe Salou <bezedache29@gmail.com>
2021-04-17 17:46:56 +0200

    sylisé l'edit de details

--- src/components/ResourceDetails.vue
+++ src/components/ResourceDetails.vue
@@ -3,19 +3,33 @@
   <div class="details">
     <h1>Détails de la ressource : {{ resource.title }}</h1>
     <h3>Description :</h3>
-    <p>{{ resource.desc }}</p>
+    <p v-if="!isEdit">{{ resource.desc }}</p>
+    <div v-else class="group">
+      <textarea class="textarea" v-model.trim="desc" name="desc" cols="30" rows="10" @keyup.enter="changeDesc"></textarea>
+    </div>
     <h3>Commentaire :</h3>
-    <p>{{ resource.commentary }}</p>
-    <div class="buttons">
-        <a target="_blank" :href="resource.link">{{ resource.link }}</a>
-      </div>
+    <p v-if="!isEdit">{{ resource.commentary }}</p>
+    <div v-else class="group">
+      <textarea class="textarea" v-model.trim="commentary" name="desc" cols="30" rows="10" @keyup.enter="changeCommentary"></textarea>
+    </div>
+    <div class="buttons" v-if="!isEdit">
+      <a target="_blank" :href="resource.link">{{ resource.link }}</a>
+    </div>
+    <div v-else class="group group-link">
+      <h3>Link :</h3>
+      <input class="input" v-model.trim="link" type="text" @keyup.enter="changeLink" />
+    </div>
+    <div class="btn">
+      <button @click="edit" v-if="!isEdit">Edit</button>
+      <button @click="change" v-else class="btn-change">Change</button>
+    </div>
   </div>
 
 </template>
 
 <script>
 
-import axios from 'axios';
+import axios from 'axios'
 
 
 export default {
@@ -29,6 +43,10 @@ export default {
   data() {
     return {
       resource: {},
+      isEdit: false,
+      desc: '',
+      commentary: '',
+      link: ''
     }
   },
 
@@ -46,17 +64,27 @@ export default {
 
   methods: {
     async getCommentary(id) {
-      const url = `https://resources-vuejs-default-rtdb.europe-west1.firebasedatabase.app/resources`;
+      const url = `https://resources-vuejs-default-rtdb.europe-west1.firebasedatabase.app/resources`
       try {
-        const response = await axios.get(`${url}/${id}.json`);
+        const response = await axios.get(`${url}/${id}.json`)
         this.resource = response.data
+        this.desc = response.data.desc
+        this.commentary = response.data.commentary
+        this.link = response.data.link
         console.log(this.resource)
 
       } catch(error) {
-        alert(error.message);
+        alert(error.message)
       }
     },
 
+    edit() {
+      this.isEdit = true
+    },
+
+    change() {
+      this.isEdit = false
+    }
     
 
   }
@@ -73,7 +101,8 @@ export default {
     border-radius: 15px;
     margin-top: 10px;
     margin-left: 2.5%;
-    max-height: 600px;
+    /* max-height: 600px; */
+    height: 100%;
   }
   h1 {
     text-align: center;
@@ -96,5 +125,27 @@ export default {
   p, h3 {
     padding-left: 20px;
   }
-  
+  .group {
+    display: flex;
+    justify-content: center;
+  }
+  .textarea {
+    width: 95%;
+  }
+  .group-link {
+    display: flex;
+    flex-direction: column;
+    margin: 0 20px 20px 20px;
+  }
+  .btn-change {
+    margin: 0 20px 20px 20px;
+    padding: 10px 20px;
+    cursor: pointer;
+    background-color: blueviolet;
+    border: none;
+    color: white;
+  }
+  .btn-change:hover {
+    background-color: red;
+  }
 </style>
